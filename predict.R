293e367055656dac1dcc81ca0c6e928b7d50ec5c
nattsp <drnphillips@gmail.com>
2017-11-29 18:51:54 +1100

    more work on the functions

--- predict.R
+++ predict.R
@@ -42,10 +42,46 @@ ngramPredict <- function(xt, ngramDT){
 
 predictWordFromNgrams <- function(quin, quad, tri, bi){
     l = list(quin, quad, tri, bi)
+    noGuess <- 10
     wordsTemp = as.data.table(rbindlist(l))
-    words = wordsTemp[, .(score = max(prob)), by = .(predict)][1:5]
+    words = wordsTemp[, .(score = max(prob)), by = .(predict)][1:noGuess]
     words
 }
+
+ngramGenerator <- function(xt){
+    txtCorpus <- tokensFun(xt)
+    nWords <- ntoken(txtTokens)[[1]]
+    if (nWords > 3){
+        txtQuad <- preceedingWords(txtCorpus, 4)
+    } else { txtQuad <- "a a a a"}
+    if (nWords >2){
+        txtTri <- preceedingWords(txtCorpus, 3)
+    } else {txtTri <- "a a a"}
+    txtBi <- preceedingWords(txtCorpus, 2)
+    txtUni <- preceedingWords(txtCorpus, 1)
+    list(txtQuad = txtQuad, txtTri = txtTri, txtBi = txtBi, txtUni = txtUni)
+}
+
+ngramsPredict <- function(ngrams, quingramDT, quadgramDT, trigramDT, bigramDT){
+    quinPredict <- ngramPredict(ngrams$txtQuad, quingramDT)
+    quadPredict <- ngramPredict(ngrams$txtTri, quadgramDT)
+    triPredict <- ngramPredict(ngrams$txtBi, trigramDT)
+    biPredict <- ngramPredict(ngrams$txtUni, bigramDT)
+    
+    print(quinPredict)
+    print(quadPredict)
+    print(triPredict)
+    print(biPredict)
+    
+    quadPredict[, prob := 0.4 * prob]
+    triPredict[, prob := 0.4 * 0.4 * prob]
+    biPredict[, prob := 0.4 * 0.4 * 0.4 * prob]
+    
+    predictWord <- predictWordFromNgrams(quinPredict, quadPredict, triPredict, biPredict)
+    return(predictWord)
+}
+
+
 
 
 # Test input text
@@ -56,6 +92,9 @@ txt = "Hey sunshine, can you follow me and make me the"
 txt = "Very early observations on the Bills game: Offense still struggling but the"
 txt = "Go on a romantic date at the"
 txt = "Well I'm pretty sure my granny has some old bagpipes in her garage I'll dust them off and be on my"
+txt = "Ohhhhh #PointBreak is on tomorrow. Love that film and haven't seen it in quite some"
+txt = "After the ice bucket challenge Louis will push his long wet hair out of his eyes with his little"
+txt = "Be grateful for the good times and keep the faith during the"
 
 ## Convert to ngrams using the last words in the sentence
 txtCorpus <- corpus(txt)
@@ -155,4 +194,11 @@ triPredict <- ngramPredict(txtBi, trigramDT)
 biPredict <- ngramPredict(txtUni, bigramDT)
 
 predictWord <- predictWordFromNgrams(quinPredict, quadPredict, triPredict, biPredict)
+
+### Even more steps in functions
+
+l <- ngramGenerator(txt)
+
+predictWord <- ngramsPredict(l, quingramDT, quadgramDT, trigramDT, bigramDT)
+
 predictWord
