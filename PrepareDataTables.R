cfae71f839e98f2761af56dcc5685a0ffb2474d3
nattsp <drnphillips@gmail.com>
2017-11-29 13:19:41 +1100

    get probabilility of one ngram given the n-1gram

---
+++ PrepareDataTables.R
@@ -0,0 +1,9 @@
+## Prepare data.tables
+
+load(file = "../../Data/quingramDT.RData")
+load(file = "../../Data/quadgramDT.RData")
+load(file = "../../Data/trigramDT.RData")
+load(file = "../../Data/bigramDT.RData")
+load(file = "../../Data/unigramDT.RData")
+
+load(file = "../../Data/profanity.RData")
