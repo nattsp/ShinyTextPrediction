90d56e61a2f5e5925b64867bd69ec53cf2729f68
nattsp <drnphillips@gmail.com>
2017-11-29 16:23:20 +1100

    Prediction complete

--- PrepareDataTables.R
+++ PrepareDataTables.R
@@ -40,4 +40,76 @@ quadgramDT[phrase == "is the time"]
 dim(quingramDT)
 dim(quadgramDT)
 
-#check git problems
+
+## Predict
+
+# Test input text
+txt = "The guy in front of me just bought a pound of bacon, a bouquet, and a case of"
+txt = "#greatday this is a tweet fuck"
+
+## Convert to ngrams using the last words in the sentence
+txtCorpus <- corpus(txt)
+
+txtTokens <- tokens(txtCorpus
+                    #, tolower = TRUE
+                    , remove_numbers = TRUE
+                    , remove_punct = TRUE
+                    , remove_separators = TRUE
+                    , remove_twitter = TRUE
+                    , remove_url = TRUE
+                    , verbose = TRUE)
+
+head(txtTokens)
+
+txtTokens <- tokens_select(txtTokens
+                           , profanity
+                           , selection = "remove"
+                           , verbose = quanteda_options("verbose"))
+
+wordCount <- ntoken(txtTokens)
+
+# Prepare the text
+# Need to match the ngram data.tables
+txtQuad <- tokens_ngrams(txtTokens, n = 4, concatenator = " ")
+txtTri <- tokens_ngrams(txtTokens, n = 3, concatenator = " ")
+txtBi <- tokens_ngrams(txtTokens, n = 2, concatenator = " ")
+txtQuad
+txtTri
+txtUni
+txtQuad <- tail(txtQuad[[1]], 1)
+txtTri <- tail(txtTri[[1]], 1)
+txtBi <- tail(txtBi[[1]], 1)
+txtUni <- tail(txtTokens[[1]], 1)
+
+
+quinPredict <- quingramDT[phrase == txtQuad][order(-prob)]
+quadPredict <- quadgramDT[phrase == txtTri][order(-prob)]
+triPredict <- trigramDT[phrase == txtBi][order(-prob)]
+biPredict <- bigramDT[phrase == txtUni][order(-docfreq)]
+
+quinPredict[, c("ngram", "phrase", "docfreq") := NULL]
+quadPredict[, c("ngram", "phrase", "docfreq") := NULL]
+triPredict[, c("ngram", "phrase", "docfreq") := NULL]
+biPredict[, c("ngram", "phrase", "docfreq") := NULL]
+
+quadPredict[, prob := 0.4 * prob]
+triPredict[, prob := 0.4 * 0.4 * prob]
+biPredict[, prob := 0.4 * 0.4 * 0.4 * prob]
+
+quinPredict
+quadPredict
+triPredict
+biPredict
+
+l = list(quinPredict, quadPredict, triPredict, biPredict)
+predictWordTemp = as.data.table(rbindlist(l))
+predictWord = predictWordTemp[, .(prob = max(prob)), by = .(predict)][1:5]
+predictWord
+
+
+
+
+class(predictWord)
+key(predictWord)
+key(quinPredict)
+key(quingramDT)
--- testing.R
+++ testing.R
@@ -1,3 +1,30 @@
 DT <- data.table(x = sample(LETTERS[1:5], 20, TRUE), key = "x"); DT
 DT[  , .I[x == "E"] ] 
 DT[J("E")  , .I]
+
+
+quingramTrain <- dfm(
+    TrainSentences
+    , tolower = TRUE
+    , remove_numbers = TRUE
+    , remove_punct = TRUE
+    , stem = FALSE
+    , ngrams = 5
+    , remove_twitter = TRUE
+    , remove_url = TRUE
+    , concatenator = " "
+    , verbose = TRUE)
+
+# Test input text
+txt = "The guy in front of me just bought a pound of bacon, a bouquet, and a case of"
+txt = "#greatday this is a tweet fuck"
+
+## Convert to ngrams using the last words in the sentence
+txtCorpus <- corpus(txt)
+
+txtTokens <- tokens(txtCorpus
+                    , remove_numbers = TRUE
+                    , remove_punct = TRUE
+                    , remove_separators = TRUE
+                    , remove_twitter = TRUE)
+head(txtTokens)
